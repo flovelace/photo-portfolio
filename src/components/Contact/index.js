de389c314b726c97470a1161aa5d72f98f809cbc
fairfax <lovelace.af@gmail.com>
2022-03-12 23:01:05 -0500

    completed form with state

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,27 +4,30 @@ function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
 
-    function handlechange(e) {
+    function handleChange(e) {
         setFormState({...formState, [e.target.name]: e.target.value })
     }
 
-    console.log(formState);
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
 
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id ="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onChange={handlechange} name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" defaultValue={email} name="email" />
+                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                     <button type="submit">Submit</button>
                 </div>
             </form>
